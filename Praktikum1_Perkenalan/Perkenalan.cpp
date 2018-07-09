8f82c5aa9f4a74d39d80e9f5818c4be0088b7172
ahmdsyaukani <40911736+ahmdsyaukani@users.noreply.github.com>
2018-07-09 07:14:54 +0700

    Update Perkenalan.cpp

--- Praktikum1_Perkenalan/Perkenalan.cpp
+++ Praktikum1_Perkenalan/Perkenalan.cpp
@@ -2,18 +2,11 @@
 #include <conio.h>
 using namespace std;
 int main(){
-	float nilai1, nilai2, nilaiA;
-	
-	cout<<"Cek Nilai Akhir"<<endl;
-	cout<<"Nilai Materi Struktur Data : ";
-	cin>>nilai1;
-	cout<<"Nilai Praktek Struktur Data : ";
-	cin>>nilai2;
-	
-	nilaiA=(nilai1+nilai2)/2;
-	
-	cout<<"Nilai Akhir Matkul Struktur Data : "<<nilaiA;
+	cout<<"Hello, Nama saya Ahmad Syaukani"<<endl;
+	cout<<"Mahasiswa Esa Unggul Nih !!!";
+	getch();
 }
+
 
 
 
